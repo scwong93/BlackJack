28d319893a0f56477394c419c640f7cd7791f8e6
Simon <scwong93@gmail.com>
2019-02-01 10:40:02 -0800

    move classes into main file

--- blackjack.py
+++ blackjack.py
@@ -10,6 +10,65 @@ values = {'Ace': 11, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seve
 
 playing = True
 
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for i in self.deck:
+            deck.comp += '\n' + i.__str__()
+        return 'The deck has: ' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Chips:
+
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
 
 def take_bet(chips):
     while True:
@@ -95,7 +154,6 @@ while True:
     print('Welcome to the game of BlackJack!')
 
 
-    # Create & shuffle the deck, deal two cards to each player
     deck = Deck()
     deck.shuffle()
 
@@ -107,43 +165,29 @@ while True:
     dealer_hand.add_card(deck.deal())
     dealer_hand.add_card(deck.deal())
 
-    # Set up the Player's chips
-
     player_chips = Chips()
 
-    # Prompt the Player for their bet
-
     take_bet(player_chips)
-
-    # Show cards (but keep one dealer card hidden)
 
     show_some(player_hand,dealer_hand)
 
 
-    while playing:  # recall this variable from our hit_or_stand function
+    while playing:
 
-        # Prompt for Player to Hit or Stand
         hit_or_stand(deck, player_hand)
 
-
-        # Show cards (but keep one dealer card hidden)
         show_some(player_hand,dealer_hand)
 
-        # If player's hand exceeds 21, run player_busts() and break out of loop
         if player_hand.value > 21:
-            player_busts(player_hand, dealer_hand, chips)
+            player_busts(player_hand, dealer_hand, player_chips)
             break
 
-    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
     if player_hand.value <= 21:
         while dealer_hand.value < 17:
             hit(deck, dealer_hand)
 
-        # Show all cards
         show_all(player_hand, dealer_hand)
 
-
-        # Run different winning scenarios
         if dealer_hand.value > 21:
             dealer_busts(player_hand, dealer_hand, player_chips)
         elif player_hand.value > 21:
@@ -155,10 +199,8 @@ while True:
         else:
             push(player_hand,dealer_hand)
 
-    # Inform Player of their chips total
     print('Your current chips total is {}'.format(player_chips.total))
 
-    # Ask to play again
     new_game = input('Would you like to play another round? [Y/N]: ')
 
     if new_game[0].lower() == 'y':
